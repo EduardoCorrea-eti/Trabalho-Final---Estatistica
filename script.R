a0fb10bf52664cdf49660224069f1bc49c4cc33a
Carlos Eduardo Corrêa <carloseducorrea10@gmail.com>
2026-05-06 03:59:54 -0300

    Iniciando coleta de dados

--- script.R
+++ script.R
@@ -8,5 +8,52 @@ options(scipen = 999, digits = 4)
 #Fecha janelas graficas abertas em execuções anteriores
 graphics.off()
 
+#=========================================================================
+# 2.2 Coleta de Dados
+#=========================================================================
+
+# a) Cosntrução do Dataframe
+#----------------------------
+
+  #recebe o link de download da tabela
+  url_csv <- "https://drive.google.com/uc?export=download&id=12_MRBwS1QP26HpwuVAJr4cb-gylc0m5d"
+  
+  #recebe a leitura do arquivo
+  criminalidade_sp <- readr::read_csv(url_csv)
+  
+  # Seleção do Estado
+  estado <- "SP" 
+  
+  df_base <- criminalidade_sp %>%
+    filter(uf == estado) %>%
+  # Garantir que o código seja tratado como texto para evitar perda de zeros à esquerda
+    mutate(cod_municipio = as.character(cod_municipio))
+  
+  # Verificar se o filtro funcionou
+  print(paste("Total de Municípios no estado selecionado:", nrow(df_base)))
+  
+#b)Ulilizar o pacote SIDRAR
+#---------------------------
+
+#=========================================================================
+# 2.3 Construção do Modelo
+#=========================================================================
+
+#=========================================================================
+# 2.4 Análise do Modelo
+#=========================================================================
+
+#=========================================================================
+# 2.5 Ajuste do Modelo
+#=========================================================================
+
+#=========================================================================
+# 2.6 Simplificando o Modelo
+#=========================================================================
+
+#=========================================================================
+# 2.7 Customizando o Modelo
+#=========================================================================
+
 
 
