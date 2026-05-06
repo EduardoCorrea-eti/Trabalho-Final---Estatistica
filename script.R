ab39f6f3b88184884557950808e94981e57fd238
Carlos Eduardo Corrêa <carloseducorrea10@gmail.com>
2026-05-06 01:23:26 -0300

    Primeiro comit: Configurações iniciais

---
+++ script.R
@@ -0,0 +1,12 @@
+#=======================LIMPEZA DO ABLIENTE=====================================
+#limpa objetos do workspace e previne conflitos
+rm(list = ls())
+
+#Evita notação cientifica e melhora a legibilidade numérica no console
+options(scipen = 999, digits = 4)
+
+#Fecha janelas graficas abertas em execuções anteriores
+graphics.off()
+
+
+
